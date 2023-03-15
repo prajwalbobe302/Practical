dec9772960204e873f5c05f9c8006adc6b705d57
prajwalbobe302 <127470337+prajwalbobe302@users.noreply.github.com>
2023-03-15 13:26:53 +0530

    Add files via upload

---
+++ AreaofRectangle.java
@@ -0,0 +1,8 @@
+class AreaofRectangle{
+	public static void main (String[] args)
+{
+	System.out.println("Enter the length of Rectangle:");
+	System.out.println("Enter the width of Rectangle:");
+	int area=lenght*breadth;
+	System.out.println("Area of Rectangle is:"+area);
+}}
---
+++ Perimeter.java
@@ -0,0 +1,9 @@
+class Perimeter {
+   public static void main (String[] args)
+   {
+	   int length = 4;
+	   int width = 8;
+	   int perimeter = 2*(length*width);
+	   System.out.println("Perimeter is:"+perimeter);
+   }
+}
