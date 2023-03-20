b2f7bb1fad3488e65abd874c70141c535435e111
prajwalbobe302 <127470337+prajwalbobe302@users.noreply.github.com>
2023-03-20 12:57:27 +0530

    Add files via upload

---
+++ WhileloopEven.java
@@ -0,0 +1,11 @@
+//1st 10 Even numbers
+class WhileloopEven {
+public static void main(String[]args) {
+int i=0;
+System.out.println("First 10 even numbers \n");
+while(i<=10) {
+System.out.println(i);
+i=i+2;
+}
+}
+} 
---
+++ WhileloopOdd.java
@@ -0,0 +1,12 @@
+//1st 10 Odd numbers
+class WhileloopOdd {
+public static void main(String[]args) {
+int i=1;
+System.out.println("First 10 Odd numbers \n");
+while(i<=10)
+ {
+System.out.println(2*i-1);
+	i++;
+}
+}
+} 
