fea88f801744dde87c2f782e39696fa225569629
prajwalbobe302 <127470337+prajwalbobe302@users.noreply.github.com>
2023-04-25 01:23:27 +0530

    Add files via upload

---
+++ Duplicate_Array.java
@@ -0,0 +1,27 @@
+package Array;
+/*Problem 4
+ * Write a Java program to find the duplicate values of an array of integer values.
+ */
+public class Duplicate_Array {
+		
+		    public static void main(String[] args) {
+		    	
+		    	//int[] arr = {2, 3, 4, 5, 2, 4, 5, 7, 8};
+		    	int[] arr = {5,2,7,7,5};
+		    	
+		    	System.out.println("The actual array is:");
+		    	for(int i:arr)//for each loop for print 
+		    	System.out.println(i);
+		    	
+		    	// Find duplicates in the array
+		        for (int i=0; i<arr.length-1; i++) {
+		            for (int j = i + 1; j < arr.length; j++) {
+		                if (arr[i] == arr[j]) {
+		                    System.out.println("The Duplicate Array is: " + arr[i]);
+		                }
+		            }
+		        }
+		    }
+		}
+		    
+
